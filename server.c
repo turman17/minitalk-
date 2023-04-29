c708797f93f651eebdec3b64b9fbe3d4ffac777f
turman17 <70319205+turman17@users.noreply.github.com>
2023-04-29 10:54:08 +0100

    Update server.c

--- server.c
+++ server.c
@@ -10,81 +10,83 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include<signal.h>
-#include<unistd.h>
-#include<stdio.h>
-#include<sys/types.h>
-#include<stdlib.h>
-#include <limits.h>
 
-void	ft_putchar(int c)
-{
-	write (1, &c, 1);
-}
+	#include <limits.h>
+	#include <signal.h>
+	#include <stdio.h>
+	#include <stdlib.h>
+	#include <sys/types.h>
+	#include <unistd.h>
 
-void	ft_putnbr(int num)
-{
-	if (num > 9)
+	void	ft_putchar(int c)
 	{
-		ft_putnbr(num / 10);
-		num = num % 10;
+		write(1, &c, 1);
 	}
-	if (num <= 9)
-		ft_putchar (('0' + num));
-}
 
-void	conv_txt(char *s)
-{
-	int		i;
-	int		base;
-	char	bit;
-	int		control;
-	int		result;
-
-	i = 7;
-	while (s[i])
+	void	ft_putnbr(int num)
 	{
-		base = 1;
-		control = i;
-		result = 0;
-		while (base <= 128)
+		if (num > 9)
 		{
-			bit = s[control];
-			if (bit == '1')
-				result = result + base;
-			base = base * 2;
-			control--;
+			ft_putnbr(num / 10);
+			num = num % 10;
 		}
-		write(1, &result, 1);
-		i = i + 8;
+		if (num <= 9)
+			ft_putchar(('0' + num));
 	}
-}
 
-void	alm_bin(int sig)
-{
-	static int	i = 0;
-	static char	c[8];
-
-	if (sig == SIGUSR1)
-		c[i] = '1';
-	else if (sig == SIGUSR2)
-		c[i] = '0';
-	i++;
-	if (i == 8)
+	void	conv_txt(char *s)
 	{
-		i = 0;
-		conv_txt(c);
-	}
-}
+		int		i;
+		int		base;
+		char	bit;
+		int		control;
+		int		result;
 
-int	main(void)
-{
-	getpid();
-	ft_putnbr(getpid());
-	write(1, "\n", 1);
-	signal(SIGUSR1, alm_bin);
-	signal(SIGUSR2, alm_bin);
-	while (1)
-		sleep(1);
-	return (0);
+		i = 7;
+		while (s[i])
+		{
+			base = 1;
+			control = i;
+			result = 0;
+			while (base <= 128)
+			{
+				bit = s[control];
+				if (bit == '1')
+					result = result + base;
+				base = base * 2;
+				control--;
+			}
+			write(1, &result, 1);
+			i = i + 8;
+		}
+	}
+
+	void	alm_bin(int sig)
+	{
+		static int	i;
+		static char	c[8];
+
+		i = 0;
+		if (sig == SIGUSR1)
+			c[i] = '1';
+		else if (sig == SIGUSR2)
+			c[i] = '0';
+		i++;
+		if (i == 8)
+		{
+			i = 0;
+			conv_txt(c);
+		}
+	}
+
+	int	main(void)
+	{
+		getpid();
+		ft_putnbr(getpid());
+		write(1, "\n", 1);
+		signal(SIGUSR1, alm_bin);
+		signal(SIGUSR2, alm_bin);
+		while (1)
+			sleep(1);
+		return (0);
 }
